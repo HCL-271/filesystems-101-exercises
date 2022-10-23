f0673458c7791d5d1bdd21dac9574a16122ed1d3
H-Chlor <32020828+HCL-271@users.noreply.github.com>
2022-10-23 21:44:46 +0300

    Update solution.c

--- 02-fuse-helloworld/solution.c
+++ 02-fuse-helloworld/solution.c
@@ -1,9 +1,15 @@
-
+#include "solution.h"
 
 #include <fuse.h>
 
+static void *init_hello(struct fuse_conn_info *fuse_conn_info, struct fuse_config *fuse_config) {
+  (void)fuse_conn_info;
+  (void)fuse_config;
+  return NULL;
+}
+
 static const struct fuse_operations hellofs_ops = {
-	/* implement me */
+	    .init = init_hello
 };
 
 int helloworld(const char *mntp)
