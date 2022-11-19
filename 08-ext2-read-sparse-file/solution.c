828190c57cd8cd03a4bd7751f339113b4ca9a535
H-Chlor <32020828+HCL-271@users.noreply.github.com>
2022-11-19 21:45:11 +0300

    Update solution.c

--- 08-ext2-read-sparse-file/solution.c
+++ 08-ext2-read-sparse-file/solution.c
@@ -66,12 +66,13 @@ static __u32 size;
 static __u32 off_tab = 0;
 int copying_cur_buff(int img, int out, __le32 block_nr)
 {
+	__u32 arrayж
 	if (bites_in_blk<size-off_tab)
 	{
-		__u32 array = bites_in_blk;
+		 array = bites_in_blk;
 	}else
 	{
-		__u32 array = size-off_tab;
+		 array = size-off_tab;
 	}
 	//__u32 array = bites_in_blk<size-off_tab?bites_in_blk:size-off_tab;
 	if(array == 0)
@@ -157,7 +158,7 @@ int dump_file(int img, int inode_nr, int out)
 	int inode_in_group = (inode_nr - 1) % ext2_super_block1.s_inodes_per_group;
 
 	off_tab = bites_in_blk * ext2_group_desc1.bg_inode_table + inode_in_group * ext2_super_block1.s_inode_size;
-	array  = pread(img, &in, sizeof(in), off_tab);
+	array  = pread(img, &ext2_inode1, sizeof(ext2_inode1), off_tab);
 	if(array < 0){
 		return -errno;
 	}
@@ -171,14 +172,14 @@ int dump_file(int img, int inode_nr, int out)
 			return back;
 		}
 	}
-	single_inderect_block_buf = (__le32*)malloc(bites_in_blk);
+	copy_si_buffer_size = (__le32*)malloc(bites_in_blk);
 	//13th block is single-indirect
 	back = copy_si_buffer_size(img, out, ext2_inode1.i_block[12]);
 	if(back < 0){
 		return back;
 	}
 	di_buffer_size = (__le32*)malloc(bites_in_blk);
-	back = copy_double_indirect_block(img, out, ext2_inode1.i_block[13]);
+	back = copy_di_buffer_size(img, out, ext2_inode1.i_block[13]);
 	if(back < 0)
 	{
 		return back;
