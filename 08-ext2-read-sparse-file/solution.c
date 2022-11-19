f3b8b725455c81cfade8d981cc97297833cf50fd
H-Chlor <32020828+HCL-271@users.noreply.github.com>
2022-11-19 21:56:43 +0300

    Update solution.c

--- 08-ext2-read-sparse-file/solution.c
+++ 08-ext2-read-sparse-file/solution.c
@@ -66,7 +66,7 @@ static __u32 size;
 static __u32 off_tab = 0;
 int copying_cur_buff(int img, int out, __le32 block_nr)
 {
-	__u32 array = __u32 offset = 1024; <size-copy_offset?__u32 offset = 1024; :size-off_tab;
+	__u32 array = bites_in_blk<size-off_tab?bites_in_blk:size-off_tab;
 	if(array == 0){
 		return 0;
 	}
