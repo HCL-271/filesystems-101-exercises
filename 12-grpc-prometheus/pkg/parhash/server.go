d3f4c94c091870143e3f74e658f1d98c8816cda2
H-Chlor <32020828+HCL-271@users.noreply.github.com>
2022-12-11 21:02:30 +0300

    Update server.go

--- 12-grpc-prometheus/pkg/parhash/server.go
+++ 12-grpc-prometheus/pkg/parhash/server.go
@@ -2,10 +2,24 @@ package parhash
 
 import (
 	"context"
-
+	"time"
+		
+	"net"
+	"sync"
+	
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
+	"google.golang.org/grpc"
+	hashpb "fs101ex/pkg/gen/hashsvc"
 	"golang.org/x/sync/semaphore"
+
+	parhashpb "fs101ex/pkg/gen/parhashsvc"
+
+	"fs101ex/pkg/workgroup"
+
+
+
+
 )
 
 type Config struct {
@@ -53,7 +67,17 @@ type Config struct {
 // Both performance counters must be placed to Prometheus namespace "parhash".
 type Server struct {
 	conf Config
+	MutexSyncronizer sync.Mutex
+	checker int
+	previous int
+	
+	Pcounter prometheus.Counter 
+	Histvec *prometheus.HistogramVec
 
+	stop context.CancelFunc
+	l    net.Listener
+	wg   sync.WaitGroup
+	
 	sem *semaphore.Weighted
 }
 
@@ -61,22 +85,146 @@ func New(conf Config) *Server {
 	return &Server{
 		conf: conf,
 		sem:  semaphore.NewWeighted(int64(conf.Concurrency)),
+		counter: prometheus.NewCounter(prometheus.CounterOpts
+					       {
+			Namespace: "parhash",
+			Name: "nr_requests",
+			Help: "Number of requests",
+		}),
+		subquery_durations: prometheus.NewHistogramVec(prometheus.HistogramOpts
+							       {
+			Namespace: "parhash",
+			Name: "subquery_durations",
+			Help: "Subquery durations",
+			Buckets: prometheus.ExponentialBuckets(0.1, 1e4, 24),
+		},
+							       []string{"backend"}
+							      ),
+		checker: 0,
 	}
 }
-
+//copy from hash file
 func (s *Server) Start(ctx context.Context) (err error) {
-	defer func() { err = errors.Wrap(err, "Start()") }()
+	defer func() { err = errors.Wrapf(err, "Start()") }()
 
-	/* implement me */
+	ctx, s.stop = context.WithCancel(ctx)
+
+	s.l, err = net.Listen("tcp", s.conf.ListenAddr)
+	if err != nil {
+		return err
+	}
+	srv := grpc.NewServer()
+	parhashpb.RegisterParallelHashSvcServer(srv, s)
+	/* We can initialize new here as:
+	s.Pcounter = prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: "parhash",
+			Name: "nr_requests",
+			Help: "Number of requests",
+	})
+	
+	s.Histvec = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Namespace: "parhash",
+			Name: "subquery_durations",
+			Help: "Subquery durations",
+			Buckets: prometheus.ExponentialBuckets(0.1, 1e4, 24),}, []string{"backend"},)
+	
+	and keep this function below without changing 
+	but that way makes warning on my system
+	
+	func New(conf Config) *Server {
+	return &Server{
+		conf: conf,
+		sem:  semaphore.NewWeighted(int64(conf.Concurrency)),
+	}
+}
+	
+	*/
+	
+	s.conf.Prom.MustRegister(s.counter)
+	s.conf.Prom.MustRegister(s.subquery_durations)
+
+	s.wg.Add(2)
+	go func() {
+		defer s.wg.Done()
+
+		srv.Serve(s.l)
+	}()
+	go func() {
+		defer s.wg.Done()
+
+		<-ctx.Done()
+		s.l.Close()
+	}()
 
 	return nil
 }
+//zero IQ cope from hash file code
 
 func (s *Server) ListenAddr() string {
-	/* implement me */
-	return ""
+	return s.l.Addr().String()
 }
-
+//zero IQ cope from hash file code
 func (s *Server) Stop() {
-	/* implement me */
+	s.stop()
+	s.wg.Wait()
+}
+// let's take that code from 11-grpc task
+func (s *Server) ParallelHash(ctx context.Context, req *parhashpb.ParHashReq) (resp *parhashpb.ParHashResp, err error) {
+	defer func() { err = errors.Wrapf(err, "ParallelHash()") }()
+	
+	s.checker++
+	clients := make([]hashpb.HashSvcClient, len(s.conf.BackendAddrs))
+	joins := make([]*grpc.ClientConn, len(s.conf.BackendAddrs))
+	//this part is the same to previous task
+	for i, addr := range s.conf.BackendAddrs {	
+		joins[i], err = grpc.Dial(addr, grpc.WithInsecure())
+		if err != nil {
+			return nil, err
+		}
+		defer joins[i].Close()
+		clients[i] = hashpb.NewHashSvcClient(joins[i])
+	}
+	
+
+	var (
+		workgroup1     = workgroup.New(workgroup.Config{Sem: s.sem})
+		hashes = make([][]byte, len(req.Data))
+	)
+	
+	for i := range req.Data {
+		number := i
+		workgroup1.Go(ctx, func(ctx context.Context) error {
+			from0 = time.Now()
+			s.MutexSyncronizer.Lock()
+			previous := s.checker
+			s.checker ++
+			if s.checker >= len(s.conf.BackendAddrs) {
+				s.checker = 0
+			}
+			s.MutexSyncronizer.Unlock()
+			to0 = time.Since(from0)
+			from := time.Now()
+			resp, err := clients[previous].Hash(ctx, &hashpb.HashReq{Data: req.Data[number]})
+			to = time.Since(from)
+			if err != nil {
+				return err
+			}
+			time := float64(to.Seconds())
+			s.Histvec.With(prometheus.Labels{"backend": s.conf.BackendAddrs[previous]}).Observe(time)
+			s.MutexSyncronizer.Lock()
+			hashes[number] = resp.Hash
+			s.MutexSyncronizer.Unlock()
+			return nil
+		})
+	}
+
+	
+	
+	
+	
+	if err := workgroup1.Wait(); err != nil {
+		log.Fatalf("", err)
+	}
+	return &parhashpb.ParHashResp{Hashes: hashes}, nil
+
 }
